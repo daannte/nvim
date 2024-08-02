100273afba258b31ed5cc854d371ac2cb4ffd5e6
Dante <dkirsman@proton.me>
2024-08-02 10:40:01 -0600

    fix(dashboard): change to mini.starter cause its more minimal

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,28 +1,16 @@
 return {
-	"goolord/alpha-nvim",
-	lazy = vim.fn.argc() ~= 0, -- only load when no file specified
+	"echasnovski/mini.starter",
+	version = false,
+	lazy = vim.fn.argc() ~= 0,
 	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
-
-		local greeting = function()
-			local hour = tonumber(vim.fn.strftime("%H"))
-			-- [04:00, 12:00) - morning, [12:00, 20:00) - day, [20:00, 04:00) - evening
-			local part_id = math.floor((hour + 4) / 8) + 1
-			local day_part = ({ "evening", "morning", "afternoon", "evening" })[part_id]
-			local username = vim.loop.os_get_passwd()["username"] or "USERNAME"
-
-			return ("Good %s, %s"):format(day_part, username)
-		end
-
-		dashboard.section.header.val = greeting()
-		dashboard.section.buttons.val = {
-			dashboard.button("f", "Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("e", "File Explorer", "<cmd>lua MiniFiles.open()<CR>"),
-			dashboard.button("q", "Quit", "<cmd>qa!<CR>"),
-		}
-		dashboard.config.opts.noautocmd = true
-
-		alpha.setup(dashboard.config)
+		require("mini.starter").setup({
+			items = {
+				{ action = "Telescope find_files", name = "Files", section = "" },
+				{ action = "lua MiniFiles.open()", name = "Browser", section = "" },
+				{ action = "qa!", name = "Quit", section = "" },
+			},
+			footer = "",
+			silent = true,
+		})
 	end,
 }
