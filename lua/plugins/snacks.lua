e44a49a872ebafc25c7e691a833d45b497d6fdff
Dante <dkirsman@gmail.com>
2025-06-08 12:50:39 -0600

    fix(icons): add nvim-web-devicons as snacks dep

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,37 +1,40 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	---@type snacks.Config
-	opts = {
-		---@class snacks.lazygit.Config: snacks.terminal.Opts
-		lazygit = {
-			config = {
-				gui = {
-					nerdFontsVersion = "",
-				},
-			},
-		},
-		picker = {
-			layout = {
-				preview = false,
-				cycle = false,
-			},
-		},
-		---@class snacks.dashboard.Config
-		dashboard = {
-			preset = {
-				keys = {
-					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-					{
-						icon = " ",
-						key = "g",
-						desc = "Find Text",
-						action = ":lua Snacks.dashboard.pick('live_grep')",
-					},
-					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
-				},
-				header = [[
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  ---@type snacks.Config
+  opts = {
+    ---@class snacks.lazygit.Config: snacks.terminal.Opts
+    lazygit = {
+      config = {
+        gui = {
+          nerdFontsVersion = "",
+        },
+      },
+    },
+    picker = {
+      layout = {
+        preview = false,
+        cycle = false,
+      },
+    },
+    ---@class snacks.dashboard.Config
+    dashboard = {
+      preset = {
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          {
+            icon = " ",
+            key = "g",
+            desc = "Find Text",
+            action = ":lua Snacks.dashboard.pick('live_grep')",
+          },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        header = [[
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⠀⠀⠀⢠⣾⣧⣤⡖⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠋⠀⠉⠀⢄⣸⣿⣿⣿⣿⣿⣥⡤⢶⣿⣦⣀⡀
@@ -47,87 +50,87 @@ return {
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀
     ]],
-			},
-			sections = {
-				{ section = "header" },
-				{ section = "keys", gap = 1, padding = 1 },
-			},
-		},
-		bigfile = { enabled = true },
-		quickfile = { enabled = true },
-		scroll = { enabled = true },
-		---@type table<string, snacks.win.Config>
-		styles = {
-			zen = {
-				relative = "editor",
-				backdrop = { transparent = false, blend = 99 },
-			},
-		},
-	},
-	keys = {
-		{
-			"<leader>gg",
-			function()
-				Snacks.lazygit()
-			end,
-			desc = "Lazygit",
-		},
-		{
-			"<leader>gl",
-			function()
-				Snacks.lazygit.log()
-			end,
-			desc = "Lazygit logs",
-		},
-		{
-			"<leader>ff",
-			function()
-				Snacks.picker.files()
-			end,
-			desc = "Find Files",
-		},
-		{
-			"<leader>fg",
-			function()
-				Snacks.picker.grep()
-			end,
-			desc = "Grep",
-		},
-		{
-			"<leader>ft",
-			function()
-				Snacks.picker.todo_comments()
-			end,
-			desc = "Todo",
-		},
-		{
-			"<leader>e",
-			function()
-				Snacks.explorer()
-			end,
-			desc = "File Explorer",
-		},
-		{
-			"gd",
-			function()
-				Snacks.picker.lsp_definitions()
-			end,
-			desc = "Goto Definition",
-		},
-		{
-			"gr",
-			function()
-				Snacks.picker.lsp_references()
-			end,
-			nowait = true,
-			desc = "References",
-		},
-		{
-			"<leader>z",
-			function()
-				Snacks.zen()
-			end,
-			desc = "References",
-		},
-	},
+      },
+      sections = {
+        { section = "header" },
+        { section = "keys",  gap = 1, padding = 1 },
+      },
+    },
+    bigfile = { enabled = true },
+    quickfile = { enabled = true },
+    scroll = { enabled = true },
+    ---@type table<string, snacks.win.Config>
+    styles = {
+      zen = {
+        relative = "editor",
+        backdrop = { transparent = false, blend = 99 },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>gg",
+      function()
+        Snacks.lazygit()
+      end,
+      desc = "Lazygit",
+    },
+    {
+      "<leader>gl",
+      function()
+        Snacks.lazygit.log()
+      end,
+      desc = "Lazygit logs",
+    },
+    {
+      "<leader>ff",
+      function()
+        Snacks.picker.files()
+      end,
+      desc = "Find Files",
+    },
+    {
+      "<leader>fg",
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = "Grep",
+    },
+    {
+      "<leader>ft",
+      function()
+        Snacks.picker.todo_comments()
+      end,
+      desc = "Todo",
+    },
+    {
+      "<leader>e",
+      function()
+        Snacks.explorer()
+      end,
+      desc = "File Explorer",
+    },
+    {
+      "gd",
+      function()
+        Snacks.picker.lsp_definitions()
+      end,
+      desc = "Goto Definition",
+    },
+    {
+      "gr",
+      function()
+        Snacks.picker.lsp_references()
+      end,
+      nowait = true,
+      desc = "References",
+    },
+    {
+      "<leader>z",
+      function()
+        Snacks.zen()
+      end,
+      desc = "References",
+    },
+  },
 }
