271c2c75602105f9c09e5543b4fded25423e604e
Dante <dkirsman@gmail.com>
2025-06-25 10:40:25 -0600

    refactor: 🔨 change up blink a bit and remove auto pairs

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,71 +1,66 @@
 return {
-	{
-		"L3MON4D3/LuaSnip",
-		event = { "InsertEnter" },
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-			config = function()
-				require("luasnip.loaders.from_vscode").lazy_load()
-			end,
-		},
-		opts = {
-			history = true,
-			delete_check_events = "TextChanged",
-		},
-	},
-	{
-		"echasnovski/mini.pairs",
-		event = "InsertEnter",
-		opts = {
-			mappings = {
-				["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
-			},
-		},
-	},
-	{
-		"saghen/blink.cmp",
-		event = "InsertEnter",
-		version = "v0.*",
-		opts = {
-			enabled = function()
-				return vim.bo.buftype == "" and vim.b.completion ~= false
-			end,
-			keymap = {
-				["<Tab>"] = {
-					function(cmp)
-						if cmp.snippet_active() then
-							return cmp.accept()
-						else
-							return cmp.select_and_accept()
-						end
-					end,
-					"snippet_forward",
-					"fallback",
-				},
-				["<S-Tab>"] = { "select_prev", "fallback" },
-				["<C-p>"] = { "select_prev", "fallback" },
-				["<C-n>"] = { "select_next", "fallback" },
-				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-			},
-			completion = {
-				menu = {
-					border = "solid",
-					draw = {
-						treesitter = { "lsp" },
-						columns = {
-							{ "kind_icon", "label", gap = 3 },
-						},
-					},
-				},
-				documentation = {
-					auto_show = true,
-					auto_show_delay_ms = 200,
-				},
-			},
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
-			},
-			cmdline = {},
-		},
-	},
+  {
+    "L3MON4D3/LuaSnip",
+    event = { "InsertEnter" },
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
+  },
+  -- {
+  --   "echasnovski/mini.pairs",
+  --   event = "InsertEnter",
+  --   opts = {
+  --     mappings = {
+  --       ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\`].", register = { cr = false } },
+  --     },
+  --   },
+  -- },
+  {
+    "saghen/blink.cmp",
+    event = "InsertEnter",
+    opts = {
+      enabled = function()
+        return vim.bo.buftype == "" and vim.b.completion ~= false
+      end,
+      keymap = {
+        ["<Tab>"] = { "accept", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      },
+      completion = {
+        menu = {
+          border = nil,
+          scrolloff = 1,
+          scrollbar = false,
+          draw = {
+            treesitter = { "lsp" },
+            columns = {
+              { "kind_icon", "label", gap = 3 },
+            },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+          window = {
+            border = nil,
+            scrollbar = false,
+            winhighlight = 'Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc',
+          },
+        },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      cmdline = {},
+    },
+  },
 }
